1946309eb117e6a54cafdbd649b7743c785b8528
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-10 20:05:20 +0100

    Add simple event handling on buton click

--- for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
+++ for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
@@ -12,10 +12,18 @@ enum IDs {
 	BUTTON_ID = 2
 };
 
+wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
+	EVT_BUTTON(BUTTON_ID, MainFrame::OnButtonClicked)
+wxEND_EVENT_TABLE()
+
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 
 	wxButton* button = new wxButton(panel, BUTTON_ID, "Button", wxPoint(300, 275), wxSize(200, 50));
 
 	CreateStatusBar();
+}
+
+void MainFrame::OnButtonClicked(wxCommandEvent& evt) {
+	wxLogStatus("Button clicked");
 }
--- for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.h
+++ for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.h
@@ -5,5 +5,8 @@ class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);
+private:
+	void OnButtonClicked(wxCommandEvent& evt);
+	wxDECLARE_EVENT_TABLE();
 };
 
