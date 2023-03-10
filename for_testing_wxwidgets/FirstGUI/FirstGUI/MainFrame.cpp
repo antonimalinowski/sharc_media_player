d7d48168a331ebda234240fea6b54ac085edcc2f
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-10 21:06:35 +0100

    Add second button - handle events both separately and commonly with event propagation and event skip

--- for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
+++ for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
@@ -11,29 +11,33 @@ Custom ID - Rules:
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 
-	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 275), wxSize(200, 50));
-	wxSlider* slider = new wxSlider(panel, wxID_ANY, 0, 0, 100, wxPoint(300, 200), wxSize(200, -1));
-	wxTextCtrl* text = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(300, 375), wxSize(200, -1));
+	wxButton* button1 = new wxButton(panel, wxID_ANY, "Button 1", wxPoint(300, 275), wxSize(200, 50));
+	wxButton* button2 = new wxButton(panel, wxID_ANY, "Button 2", wxPoint(300, 350), wxSize(200, 50));
 
-	button->Bind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
-	slider->Bind(wxEVT_SLIDER, &MainFrame::OnSliderChanged, this);
-	text->Bind(wxEVT_TEXT, &MainFrame::OnTextChanged, this);
-
-	button->Unbind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
+	this->Bind(wxEVT_CLOSE_WINDOW, &MainFrame::OnClose, this);
+	this->Bind(wxEVT_BUTTON, &MainFrame::OnAnyButtonClicked, this);
+	button1->Bind(wxEVT_BUTTON, &MainFrame::OnButton1Clicked, this);
+	button2->Bind(wxEVT_BUTTON, &MainFrame::OnButton2Clicked, this);
 
 	CreateStatusBar();
 }
 
-void MainFrame::OnButtonClicked(wxCommandEvent& evt) {
-	wxLogStatus("Button clicked");
+void MainFrame::OnClose(wxCloseEvent& evt) {
+	wxLogMessage("Frame closed");
+	evt.Skip();
 }
 
-void MainFrame::OnSliderChanged(wxCommandEvent& evt) {
-	wxString str = wxString::Format("Slider Value: %d", evt.GetInt());
-	wxLogStatus(str);
+void MainFrame::OnAnyButtonClicked(wxCommandEvent& evt) {
+	wxLogMessage("Button clicked");
 }
 
-void MainFrame::OnTextChanged(wxCommandEvent& evt) {
-	wxString str = wxString::Format("Text: %s", evt.GetString());
-	wxLogStatus(str);
+void MainFrame::OnButton1Clicked(wxCommandEvent& evt) {
+	wxLogStatus("Button 1 clicked");
+	evt.Skip();
 }
+
+void MainFrame::OnButton2Clicked(wxCommandEvent& evt) {
+	wxLogStatus("Button 2 clicked");
+	evt.Skip();
+}
+
--- for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.h
+++ for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.h
@@ -6,8 +6,9 @@ class MainFrame : public wxFrame
 public:
 	MainFrame(const wxString& title);
 private:
-	void OnButtonClicked(wxCommandEvent& evt);
-	void OnSliderChanged(wxCommandEvent& evt);
-	void OnTextChanged(wxCommandEvent& evt);
+	void OnAnyButtonClicked(wxCommandEvent& evt);
+	void OnButton1Clicked(wxCommandEvent& evt);
+	void OnButton2Clicked(wxCommandEvent& evt);
+	void OnClose(wxCloseEvent& evt);
 };
 
