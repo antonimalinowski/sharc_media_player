8f3c88eec23754936454d72e36f3fd571d6ccc98
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-10 16:22:31 +0100

    Add standard wx controls - slider, gauge('progress bar') and dropdown list

--- for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
+++ for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
@@ -7,4 +7,15 @@ MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxCheckBox* checkBox = new wxCheckBox(panel, wxID_ANY, "CheckBox", wxPoint(550, 55));
 	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, "StaticText - NOT editable", wxPoint(120, 150));
 	wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY, "TextCtrl - editable", wxPoint(500, 145), wxSize(200, -1));
+	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(100, 250), wxSize(200, -1));
+	wxGauge* gauge = new wxGauge(panel, wxID_ANY, 100, wxPoint(500, 255), wxSize(200, -1));
+	gauge->SetValue(50);
+
+	wxArrayString choices;
+	choices.Add("Item A");
+	choices.Add("Item B");
+	choices.Add("Item C");
+
+	wxChoice* choice = new wxChoice(panel, wxID_ANY, wxPoint(150, 375), wxSize(100, -1), choices);
+	choice->Select(0);
 }
