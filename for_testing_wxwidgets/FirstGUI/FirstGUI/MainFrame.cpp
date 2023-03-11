9dc5f8630ed17602f26ea10ff482d01712471609
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-11 19:53:59 +0100

    Add mouse event with position coordinates for left click

--- for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
+++ for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
@@ -11,33 +11,13 @@ Custom ID - Rules:
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 
-	wxButton* button1 = new wxButton(panel, wxID_ANY, "Button 1", wxPoint(300, 275), wxSize(200, 50));
-	wxButton* button2 = new wxButton(panel, wxID_ANY, "Button 2", wxPoint(300, 350), wxSize(200, 50));
-
-	this->Bind(wxEVT_CLOSE_WINDOW, &MainFrame::OnClose, this);
-	this->Bind(wxEVT_BUTTON, &MainFrame::OnAnyButtonClicked, this);
-	button1->Bind(wxEVT_BUTTON, &MainFrame::OnButton1Clicked, this);
-	button2->Bind(wxEVT_BUTTON, &MainFrame::OnButton2Clicked, this);
-
 	CreateStatusBar();
+
+	panel->Bind(wxEVT_LEFT_DOWN, &MainFrame::OnMouseEvent, this);
 }
 
-void MainFrame::OnClose(wxCloseEvent& evt) {
-	wxLogMessage("Frame closed");
-	evt.Skip();
+void MainFrame::OnMouseEvent(wxMouseEvent& evt) {
+	wxPoint mousePos = evt.GetPosition();
+	wxString message = wxString::Format("Mouse Event Detected (x=%d y=%d)", mousePos.x, mousePos.y);
+	wxLogStatus(message);
 }
-
-void MainFrame::OnAnyButtonClicked(wxCommandEvent& evt) {
-	wxLogMessage("Button clicked");
-}
-
-void MainFrame::OnButton1Clicked(wxCommandEvent& evt) {
-	wxLogStatus("Button 1 clicked");
-	evt.Skip();
-}
-
-void MainFrame::OnButton2Clicked(wxCommandEvent& evt) {
-	wxLogStatus("Button 2 clicked");
-	evt.Skip();
-}
-
--- for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.h
+++ for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.h
@@ -6,9 +6,6 @@ class MainFrame : public wxFrame
 public:
 	MainFrame(const wxString& title);
 private:
-	void OnAnyButtonClicked(wxCommandEvent& evt);
-	void OnButton1Clicked(wxCommandEvent& evt);
-	void OnButton2Clicked(wxCommandEvent& evt);
-	void OnClose(wxCloseEvent& evt);
+	void OnMouseEvent(wxMouseEvent& evt);
 };
 
