82150c7c8130b8e57ef1f78fc7be8662331b0b84
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-11 20:18:09 +0100

    Set mouse position in screen coordinates, convert mouse position to client coordinates

--- for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
+++ for_testing_wxwidgets/FirstGUI/FirstGUI/MainFrame.cpp
@@ -10,15 +10,18 @@ Custom ID - Rules:
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
+	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 255), wxSize(200, 100));
 
 	wxStatusBar* statusBar = CreateStatusBar();
 	statusBar->SetDoubleBuffered(true);
 
 	panel->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this);
+	button->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this);
 }
 
 void MainFrame::OnMouseEvent(wxMouseEvent& evt) {
-	wxPoint mousePos = evt.GetPosition();
+	wxPoint mousePos = wxGetMousePosition();
+	mousePos = this->ScreenToClient(mousePos);
 	wxString message = wxString::Format("Mouse Event Detected (x=%d y=%d)", mousePos.x, mousePos.y);
 	wxLogStatus(message);
 }
